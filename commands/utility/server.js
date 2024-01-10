0c7c3738ef7c935012aca48b7be18fee546ace4e
Josef Moudrik <j.moudrik@gmail.com>
2024-01-10 10:53:41 +0100

    server do not crash

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -4,6 +4,7 @@ export const data = new SlashCommandBuilder()
 	.setName('server')
 	.setDescription('Provides information about the server.');
 export async function execute(interaction) {
-	await interaction.reply({ content: `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`, ephemeral: true });
+	console.dir(interaction)
+	await interaction.reply({ content: `Sie sind aber neugierig!`, ephemeral: true });
 }
 
