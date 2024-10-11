b539a600ff5477ca1666ba1683c7cd56fc080121
Josef Moudrik <j.moudrik@gmail.com>
2024-10-12 00:06:52 +0200

    events/message: add member to row?

--- events/messages/messageCreate.js
+++ events/messages/messageCreate.js
@@ -6,14 +6,17 @@ export const data = {
     name: Events.MessageCreate,
     once: false,
     async execute(msg) {
-        const { channel, channelId, content, id, author, createdTimestamp } = msg;
+        const { channel, channelId, content, id, author, member, createdTimestamp } = msg;
         const { type: channelType, name: channelName } = channel ?? {};
         const { id: authorId, displayName, username } = author ?? {};
+        const { displayName: md, nickname : mn} = member ?? {};
 
         console.log(`messageCreate: ${author} ${displayName} ${username} -> ${channel}: "${content}"`);
         if (channelType == 11) {
             const row = {
-                authorId, channelType, channelName, channelId, content, id, createdTimestamp, displayName, username,
+                authorId, channelType, channelName, channelId, content, id, createdTimestamp,
+				displayName: md ?? mn ?? displayName,
+				username,
                 event: 'messageCreate'
             };
             await push_thread_row(channelId, row);
